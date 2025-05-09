2d91743f45628893d18bd2d321e5f9b7e1b5d694
ThariyaMDQyeen <Tharinduavishka379@gmail.com>
2025-05-09 07:37:44 +0530

    Update settings.js.thariya

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=Eth1hKaa#8YkSkxxNCl5ZEFAs6CPZ0WLZAfOV8XR-8gZVyHNTHGs",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
